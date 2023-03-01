a626ac14c01b22e4c5b54c9cae406c8b8a6f3ab8
jcw-0 <54716380+jcw-0@users.noreply.github.com>
2023-03-01 23:06:07 +0100

    Update stack.c

--- stack.c
+++ stack.c
@@ -1,32 +1,32 @@
 #include <stdint.h>
+#include <stdlib.h>
 
 typedef struct stack_t {
-  uint8_t* bot;
-  uint8_t* top;
-  uint8_t* current;
+  void* bot;
+  void* top;
+  void* current;
   size_t databsz;
 } stack_t;
 
 stack_t* stack_create(int indices, size_t databsz) {
   stack_t* s = malloc(sizeof (stack_t) + indices * databsz);
-  s->bot     = s + sizeof (stack_t);
-  s->top     = s->bot + indices * databsz;
+  s->bot     = ((uint8_t*)s) + sizeof (stack_t);
+  s->top     = ((uint8_t*)s->bot) + indices * databsz;
   s->current = s->bot;
   s->databsz = databsz;
-  
-  return s_p;
+  return s;
 }
 
 void stack_push(stack_t* s, void* data) {
   if (s->current == s->top) return;
   memcpy(s->current, data, s->databsz);
-  s->current++;
+  ((uint8_t*)s->current) + s->databsz;
 }
 
 uint8_t* stack_pop(stack_t* s) {
   if (s->current == s->bot) return;
-  s->current--;
-  return (s->current + 1)
+  ((char*)s->current) - s->databsz;
+  return ((uint8_t*)s->current + s->databsz)
 }
 
 void stack_free(stack_t* s) {
