6ba8400a6479b86e598a8aeda02f17337aa97d68
jcw-0 <54716380+jcw-0@users.noreply.github.com>
2023-03-02 13:46:09 +0100

    Update rex.c

--- rex.c
+++ rex.c
@@ -20,69 +20,68 @@ int regex(uint8_t* expression, uint8_t* input, uint8_t* output) {
 
 static int match(uint8_t* e, uint8_t* in, uint8_t* out) {
     
-	bool has_failed 		= false;
-	bool substitute 		= false;
-	bool negated 			= false;
-	uint8_t flags 			= 0;
-	uint8_t modflags 		= 0;
+    bool has_failed 	= false;
+    bool substitute 	= false;
+    bool negated 	= false;
+    uint8_t flags 	= 0;
+    uint8_t modflags 	= 0;
 	
-	uint8_t* e_before_jump = NULL;
+    uint8_t* e_before_jump = NULL;
 	
-	start_parse_loop:
-	while (!has_failed) {
+    start_parse_loop:
+    while (!has_failed) {
         switch (*e) {
-			case ' ':
-				e++;
-				break;
-			case '!':
-				negated = !negated;
-				e++;
-			break;
+	    case ' ':
+		e++;
+	    break;
+	    case '!':
+		negated = !negated;
+		e++;
+	    break;
 			
-			/* handle all expressions inside parentheses as subexpressions,
-			 * parse their content recursively */
-			case '(':
-				if (++e == '?') 
-				e_before_jump = e;
-				if (!0 == match(e, input, output)) 
-					if (*e != '|') 
-						has_failed = true;
-			break;
-			case '?':
-				if (is_function) 
-			case ')':
-				return 0;
-			case '':
-			break;
-			case '\\':
-				++e;
-				if (*e == 'n') { has_failed = ('\n' == *in)? false: true; ++e; }
-				else if (*e == 't') { has_failed = ('\t' == *in)? false: true; ++e; }
-				else if (*e == 's') { has_failed = (' '  == *in || '\t' == *in)? false: true; ++e; }
-			default:
-				has_failed = (*e == *in)? false: true;
-			break;
-		}
-		
-		has_failed = (negated)? !has_failed: has_failed;
-		has_failed = (has_failed && *(++e) == '|')? false: true;
-		
-		int match_mul = parse_match_multiplier(e);
-		switch (match_mul) {
-			case 0:
-				if (negated) { match_mul = ANY; negated = false; }
-				else negated = true;
-				
-				
-				if (!has_failed) e = e_before_subexpr;
-			break;
-			case MM_NONEORONCE:
-				goto start_parse_loop;
-			case MM_ONCEORMORE:
-				
-		}
+	    /* handle all expressions inside parentheses as subexpressions,
+	     * parse their content recursively */
+	    case '(':
+		if (++e == '?') 
+		e_before_jump = e;
+		if (!0 == match(e, input, output)) 
+		    if (*e != '|') 
+			has_failed = true;
+	    break;
+	    case '?':
+		if (is_function) 
+	    case ')':
+		return 0;
+	    case '':
+	    break;
+	    case '\\':
+		++e;
+		if (*e == 'n') { has_failed = ('\n' == *in)? false: true; ++e; }
+		else if (*e == 't') { has_failed = ('\t' == *in)? false: true; ++e; }
+		else if (*e == 's') { has_failed = (' '  == *in || '\t' == *in)? false: true; ++e; }
+	    default:
+		has_failed = (*e == *in)? false: true;
+	    break;
 	}
-	return 0;
+		
+	has_failed = (negated)? !has_failed: has_failed;
+	has_failed = (has_failed && *(++e) == '|')? false: true;
+		
+	int match_mul = parse_match_multiplier(e);
+	switch (match_mul) {
+	    case 0:
+		if (negated) { match_mul = ANY; negated = false; }
+		else negated = true;
+				
+		if (!has_failed) e = e_before_subexpr;
+	    break;
+	    case MM_NONEORONCE:
+		goto start_parse_loop;
+	    case MM_ONCEORMORE:
+	    break;
+	}
+    }
+    return 0;
 }
 
 static inline int parse_match_multiplier(uint8_t* e) {
